f57351585a72aee7dc28c74aca552b65035236c6
vaishnavi-git342 <gireeshvaishnavi0@gmail.com>
2025-09-18 02:43:08 -0700

    Add files via upload

---
+++ employ.py
@@ -0,0 +1,8 @@
+basic=float(input("enter basic pay:"))
+hra=0.10*basic
+ta=0.05*basic
+salary=basic+hra+ta
+print("basic pay:",basic)
+print("hra:",hra)
+print("ta:",ta)
+print("Total salary:",salary)
