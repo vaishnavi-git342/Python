650536b206103fdf32e1919d175461e8f173070d
vaishnavi-git342 <gireeshvaishnavi0@gmail.com>
2025-11-04 15:09:15 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,9 @@
+rows=int(input("enter the number of rows:"))
+for i in range(1,rows+1):
+    for j in range(i):
+        print("*",end= " ")
+        print()
+        for i in range(rows-1,0,-1):
+            for j in range(i):
+                print("*",end=" ")
+            print()
