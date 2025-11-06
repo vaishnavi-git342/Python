a946ad5009e4c07e805ddeab399f9d8ec8669298
vaishnavi-git342 <gireeshvaishnavi0@gmail.com>
2025-11-06 01:27:43 -0800

    Create odd.py

---
+++ odd.py
@@ -0,0 +1,9 @@
+def check(n):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+num = int(input("Enter the number: "))
+print("The given number", num, "is", check(num))
